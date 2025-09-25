8ee9156d89c2ab326d3c2e7a16b2122cc4d86697
Ali Doğan Pekdaş <147861031+AliPekdas@users.noreply.github.com>
2025-09-26 00:19:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+let meals = {
+    breakfast: [
+        { date: "15.09.2025 Pazartesi", food: "Patates Kızartması, Haşlanmış Yumurta, Beyaz Peynir, Siyah/Yeşil Zeytin, Mevsim Sebzeleri Söğüş, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" },
+        { date: "16.09.2025 Salı", food: "Peynirli Omlet, Pişi, Kaşar Peyniri, Siyah/Yeşil Zeytin, Sürülebilir Çikolata, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" },
+        { date: "17.09.2025 Çarşamba", food: "Karışık Pizza, Haşlanmış Yumurta, Krem Peynir, Siyah/Yeşil Zeytin, Mevsim Sebzeleri Söğüş, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" },
+        { date: "18.09.2025 Perşembe", food: "Menemen, Patatesli Kalem Böreği, Beyaz Peynir, Siyah/Yeşil Zeytin, Reçel Çeşitleri, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" },
+        { date: "19.09.2025 Cuma", food: "Peynirli Tepsi Böreği, Haşlanmış Yumurta, Kaşar Peyniri, Siyah/Yeşil Zeytin, Bal+Tereyağ, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" },
+        { date: "20.09.2025 Cumartesi", food: "Sucuklu Yumurta, Çikolatalı Milföy Börek, Çeçil/Örgü Peyniri, Siyah/Yeşil Zeytin, Mevsim Sebzeleri Söğüş, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" },
+        { date: "21.09.2025 Pazar", food: "Patates Kızartması, Haşlanmış Yumurta, Labne Peynir, Siyah/Yeşil Zeytin, Tahinli Pekmez, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" },
+        { date: "22.09.2025 Pazartesi", food: "Menemen, Dere Otlu Poğaça, Labne Peynir, Siyah/Yeşil Zeytin, Reçel Çeşitleri, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" },
+        { date: "23.09.2025 Salı", food: "Sosis Kokteyl, Haşlanmış Yumurta, Beyaz Peynir, Siyah/Yeşil Zeytin, Mevsim Sebzeleri Söğüş, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" },
+        { date: "24.09.2025 Çarşamba", food: "Kaşarlı Omlet, Mozaik Kek, Dil/Çeçil Peyniri, Siyah/Yeşil Zeytin, Tahinli Pekmez, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" },
+        { date: "25.09.2025 Perşembe", food: "Karışık Kızartma, Haşlanmış Yumurta, Beyaz Peynir, Siyah/Yeşil Zeytin, Mevsim Sebzeleri Söğüş, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" },
+        { date: "26.09.2025 Cuma", food: "Sade Omlet, Simit, Kaşar Peyniri, Siyah/Yeşil Zeytin, Sürülebilir Çikolata, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" },
+        { date: "27.09.2025 Cumartesi", food: "Patates Kızartması, Haşlanmış Yumurta, Beyaz Peynir, Siyah/Yeşil Zeytin, Mevsim Sebzeleri Söğüş, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" },
+        { date: "28.09.2025 Pazar", food: "Menemen, Çikolatalı Milföy Börek, Kaşar Peyniri, Siyah/Yeşil Zeytin, Reçel Çeşitleri, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" },
+        { date: "29.09.2025 Pazartesi", food: "Patatesli Kalem Böreği, Haşlanmış Yumurta, Beyaz Peynir, Siyah/Yeşil Zeytin, Mevsim Sebzeleri Söğüş, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" },
+        { date: "30.09.2025 Salı", food: "Sucuklu Yumurta, Sade Açma, Çeçil/Örgü Peyniri, Siyah/Yeşil Zeytin, Reçel Çeşitleri, 500 ml Su, Çeyrek Ekmek", calory: "650-850 kalori" }
+    ],
+    dinner: [
+        { date: "15.09.2025 Pazartesi", food: "Mercimek Çorba / Domates Çorba, Tavuklu Kağıt Kebabı / Zeytinyağlı Taze Fasulye, Şehriyeli Pirinç Pilavı, Ayran, 500 ml Su, Çeyrek Ekmek", calory: "1100-1500 kalori" },
+        { date: "16.09.2025 Salı", food: "Ezogelin Çorba / Ayranaşı Çorba, Pideli Soslu Izgara Köfte + Izgara Domates+Biber / Karışık Dolma + Yoğurt Soslu Spagetti, Kuru Fasulye Piyazı, 500 ml Su, Çeyrek Ekmek", calory: "1100-1500 kalori" },
+        { date: "17.09.2025 Çarşamba", food: "Mercimek Çorba / Şehriye Çorba, Bahçevan Kebabı / Nohut Yemeği, Bulgur Pilavı, Nektari, 500 ml Su, Çeyrek Ekmek", calory: "1100-1500 kalori" },
+        { date: "18.09.2025 Perşembe", food: "Ezogelin Çorba / Düğün Çorba, Tavuk Burger + Patates Kızartması / Mevsim Türlü, Mantı + Yoğurt, Çiğ Köfte + Marul, 500 ml Su, Çeyrek Ekmek", calory: "1100-1500 kalori" },
+        { date: "19.09.2025 Cuma", food: "Mercimek Çorba / Tarhana Çorba, İzmir Köfte / Mücver + Yoğurt, Mısırlı Pirinç Pilavı, Çoban Salata, Muzlu Magnolia, 500 ml Su", calory: "1100-1500 kalori" },
+        { date: "20.09.2025 Cumartesi", food: "Ezogelin Çorba / Cennet Çorba, Tavuk Izgara + Elma Dilim Patates / Soslu Karışık Kızartma, Makarna Çeşitleri, Yoğurt, 500 ml Su, Çeyrek Ekmek", calory: "1100-1500 kalori" },
+        { date: "21.09.2025 Pazar", food: "Mercimek Çorba / Domates Çorba, Etsiz Kuru Fasulye / Patates Köftesi + Yoğurt, Pirinç Pilavı, Cevizli Baklava, 500 ml Su, Çeyrek Ekmek", calory: "1100-1500 kalori" },
+        { date: "22.09.2025 Pazartesi", food: "Ezogelin Çorba / Havuç Çorba, Tirit Kebabı / Bezelye Yemeği, Bulgur Pilavı, Karışık Salata, 500 ml Su, Çeyrek Ekmek", calory: "1100-1500 kalori" },
+        { date: "23.09.2025 Salı", food: "Mercimek Çorba / Şehriye Çorba, Tavuk Şinitzel + Elma Dilim Patates / Zeytinyağlı Taze Fasulye, Mantı + Yoğurt, Kısır, 500 ml Su, Çeyrek Ekmek", calory: "1100-1500 kalori" },
+        { date: "24.09.2025 Çarşamba", food: "Ezogelin Çorba / Kremalı Tavuk Çorba, Kaşarlı Köfte + Izgara Domates+Biber / Zeytinyağlı Enginar Yemeği, Pirinç Pilavı, Bisküvili Pasta, 500 ml Su, Çeyrek Ekmek", calory: "1100-1500 kalori" },
+        { date: "25.09.2025 Perşembe", food: "Mercimek Çorba / Tarhana Çorba, Beşamel Soslu Tavuk / Lahana Sarma + Yoğurt, Fesleğen Soslu Makarna, Havuç Tarator, 500 ml Su, Çeyrek Ekmek", calory: "1100-1500 kalori" },
+        { date: "26.09.2025 Cuma", food: "Ezogelin Çorba / Düğün Çorba, Etsiz Kuru Fasulye / Ratatuy, Pirinç Pilavı, Şekerpare, Turşu, 500 ml Su, Çeyrek Ekmek", calory: "1100-1500 kalori" },
+        { date: "27.09.2025 Cumartesi", food: "Mercimek Çorba / Yüksük Çorba, Çin Usulü Tavuk / Karışık Sebze Graten, Soslu Spagetti, Üzüm, 500 ml Su, Çeyrek Ekmek", calory: "1100-1500 kalori" },
+        { date: "28.09.2025 Pazar", food: "Ezogelin Çorba / Şehriyeli Tavuk Çorba, Karnıyarık / Nohut Yemeği, Pirinç Pilavı, Cacık, 500 ml Su, Çeyrek Ekmek", calory: "1100-1500 kalori" },
+        { date: "29.09.2025 Pazartesi", food: "Mercimek Çorba / Domates Çorba, Çıtır Tavuk + Elma Dilim Patates / Soslu Karışık Kızartma, Soslu Makarna, Sütlaç, 500 ml Su, Çeyrek Ekmek", calory: "1100-1500 kalori" },
+        { date: "30.09.2025 Salı", food: "Ezogelin Çorba / Yayla Çorba, Hamburger + Patates Kızartması / Mücver + Yoğurt, Cevizli Erişte, Ayran, 500 ml Su, Çeyrek Ekmek", calory: "1100-1500 kalori" }
+    ]
+};
+
+let mainContainer = document.getElementById("container");
+
+function renderGrid(selectedMeal) {
+    mainContainer.innerHTML = "";
+    let grid = document.createElement("div");
+    grid.style.display = "grid";
+    grid.style.justifyContent = "center";
+    grid.style.gridTemplateColumns = "repeat(3, auto)";
+    grid.style.gap = "20px";
+
+    let container = meals[selectedMeal];
+
+    for (let i = 0; i < container.length; i++) {
+        let item = container[i];
+
+        let dayBox = document.createElement("div");
+        dayBox.style.display = "flex";
+        dayBox.style.flexDirection = "column";
+        dayBox.style.alignItems = "center";
+        dayBox.style.textAlign = "center";
+        dayBox.style.padding = "10px";
+
+        let dateDiv = document.createElement("div");
+        dateDiv.id = "date";
+        dateDiv.innerHTML = `<p id="date-cnt">${item.date}</p>`;
+
+        let foodDiv = document.createElement("div");
+        foodDiv.id = "food";
+        foodDiv.innerHTML = `<p id="food-cnt">${item.food.replace(/, /g, "<br><br>")}</p>`;
+
+        let calDiv = document.createElement("div");
+        calDiv.id = "calory";
+        calDiv.innerHTML = `<p id="calory-cnt">${item.calory}</p>`;
+
+        dayBox.appendChild(dateDiv);
+        dayBox.appendChild(foodDiv);
+        dayBox.appendChild(calDiv);
+
+        grid.appendChild(dayBox);
+
+        if (selectedMeal === "breakfast") {
+            dateDiv.style.backgroundColor = "#FFFBEB";
+            calDiv.style.backgroundColor = "#FFFBEB"; 
+        } else {
+            dateDiv.style.backgroundColor = "#EFF6FF";
+            calDiv.style.backgroundColor = "#EFF6FF";
+        }
+    }
+
+    mainContainer.appendChild(grid);
+}
+renderGrid("breakfast");
+
+const toggle = document.getElementById("mealToggle");
+
+toggle.addEventListener("change", () => {
+    if(toggle.checked){
+        renderGrid("dinner");
+    } else {
+        renderGrid("breakfast");
+    }
+});
